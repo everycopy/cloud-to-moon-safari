a6ca2a2d733930b6f8cce96317792bf2931d784e
James Goode <james@sodapopandco.com>
2013-03-13 13:42:47 +1100

    Change "cloud computing" to "moon computing" too.

--- CloudToMoon.safariextension/cloudtomoon.js
+++ CloudToMoon.safariextension/cloudtomoon.js
@@ -27,11 +27,19 @@ function walk(node) {
 function handleText(textNode)  {
 	var v = textNode.nodeValue;
 	
+	// Replace all occurences of 'the cloud' with 'the moon'
 	v = v.replace(/\bThe Cloud\b/g, "The Moon");
 	v = v.replace(/\bThe cloud\b/g, "The moon");
 	v = v.replace(/\bthe Cloud\b/g, "the Moon");
 	v = v.replace(/\bthe cloud\b/g, "the moon");
 	v = v.replace(/\bTHE CLOUD\b/g, "THE MOON");
 	
+	// Replace all occurences of 'cloud computing' with 'moon computing'
+	v = v.replace(/\bCloud Computing\b/g, "Moon Computing");
+	v = v.replace(/\bCloud computing\b/g, "Moon computing");
+	v = v.replace(/\bcloud Computing\b/g, "moon Computing");
+	v = v.replace(/\bcloud computing\b/g, "moon computing");
+	v = v.replace(/\bCLOUD COMPUTING\b/g, "MOON COMPUTING");
+	    
 	textNode.nodeValue = v;
 }
