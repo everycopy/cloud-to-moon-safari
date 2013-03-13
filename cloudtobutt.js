3d6c569a6ae54b42e205f3bb3270d65df69d7cca
James Goode <james@sodapopandco.com>
2013-03-13 12:54:17 +1100

    Changed all occurences of "my butt" to "the moon".

--- cloudtobutt.js
+++ cloudtobutt.js
@@ -27,10 +27,10 @@ function walk(node) {
 function handleText(textNode)  {
 	var v = textNode.nodeValue;
 	
-    v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-    v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+    v = v.replace(/\bThe Cloud\b/g, "The Moon");
+	v = v.replace(/\bThe cloud\b/g, "The moon");
+    v = v.replace(/\bthe Cloud\b/g, "the Moon");
+	v = v.replace(/\bthe cloud\b/g, "the moon");
 	
 	textNode.nodeValue = v;
 }
